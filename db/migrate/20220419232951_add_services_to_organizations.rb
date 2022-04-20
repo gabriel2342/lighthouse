e5d3cf963460b84275256c0b2523d1a88eb53481
gabriel2342 <gabriel.pendas@gmail.com>
2022-04-20 13:09:54 -0600

    Revert "unreverted to original last commit to try turbo frame solution"
    This reverts commit 65ffbf6d655bf65c58ede75530a62db7e56d5fb3.

--- db/migrate/20220419232951_add_services_to_organizations.rb
+++
@@ -1,9 +0,0 @@
-class AddServicesToOrganizations < ActiveRecord::Migration[7.0]
-  def self.up
-    add_column :organizations, :services, :integer, null: false, default: 0
-  end
-
-  def self.down
-    remove_column :organizations, :services
-  end
-end
