5413388f86d11b05bba25aea03656205f0478cd1
gabriel2342 <gabriel.pendas@gmail.com>
2022-04-20 13:07:41 -0600

    experimenting with turbo frame on the fly solution

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -66,6 +66,8 @@ class OrganizationsController < ApplicationController
     end
   end
 
+  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_organization
