67e4d3eb72d90b64c65291a9007391f3f84b13c9
gabriel2342 <gabriel.pendas@gmail.com>
2022-04-21 10:38:06 -0600

    added links between home and orgs

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,12 +1,16 @@
 class Organization < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :hours, dependent: :destroy
-  has_many :provided_services, dependent: :destroy
+  has_many :provided_services
   has_many :services, through: :provided_services, dependent: :destroy
   accepts_nested_attributes_for :addresses
   accepts_nested_attributes_for :hours
   accepts_nested_attributes_for :services
 
 
- 
+  # def hours_attributes=(hours_attributes)
+  #   hours_attributes.each do |i, hour_attributes|
+  #     self.addresses.build(hour_attributes)
+  #   end
+  # end
 end
